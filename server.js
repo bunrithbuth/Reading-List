0e5b7ac4d05b2dc0ec8aaee69a5dba9abf009c04
BBUTH <BBUTH_EMAIL>
2018-10-27 10:27:41 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jsx')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine('jsx', require('express-react-views').createEngine())
+
+require('./routes')(app)
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
+  
